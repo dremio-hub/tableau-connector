ae370616e65e715846456ec4ea31be86b3b22b61
Kyle Porter <kporter@dremio.com>
2020-04-26 15:02:15 -0700

    Add final Tableau impersonation changes.

--- connectionProperties.js
+++ connectionProperties.js
@@ -1,7 +1,25 @@
 (function propertiesbuilder(attr) {
+    function isEmpty(str) {
+        return (!str || 0 === str.length); 
+    }
+
     var props = {};
     props["user"] = attr[connectionHelper.attributeUsername];
     props["password"] = attr[connectionHelper.attributePassword];
+    
+    if (attr["workgroup-auth-mode"] == "db-impersonate") {
+        var str = attr[":workgroup-auth-user"];
+        
+        if (!isEmpty(str)) {
+            // Strip domain, if any
+            var arr = str.split("\\");
+            if (arr.length == 2) {
+                props["impersonation_target"] = arr[1];
+            } else {
+                props["impersonation_target"] = str;
+            }
+        }
+    }
 
     if (attr["sslmode"] !== "") {
         props["ssl"] = "true";
